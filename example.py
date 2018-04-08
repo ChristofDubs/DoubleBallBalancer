f249bc79729c5cbb4b328da695792b4fed4ad7a7
ChristofDubs <christof.dubs@gmx.ch>
2018-04-08 15:55:51 +0200

    Add sample usage script

---
+++ example.py
@@ -0,0 +1,48 @@
+"""Simple test script for 2D Double Ball Balancer
+"""
+from dynamic_model_2d import ModelParam, DynamicModel
+import matplotlib.pyplot as plt
+import numpy as np
+
+# create parameter struct
+param = ModelParam()
+param.l = 1
+param.m1 = 1
+param.m2 = 1
+param.m3 = 1
+param.r1 = 3
+param.r2 = 2
+param.theta1 = 1
+param.theta2 = 1
+param.theta3 = 1
+
+# initial state
+x0 = np.array([0, 1, 0, 0, 0, 0])
+
+# instantiate model
+model = DynamicModel(param, x0)
+
+# simulation time step
+dt = 0.05
+
+# torque
+T = 0
+
+plt.figure()
+
+# simulate until system is irrecoverable
+while not model.is_irrecoverable():
+    # simulate
+    model.simulate_step(dt, T)
+
+    # get visualization
+    vis = model.get_visualization()
+
+    # plot
+    plt.cla()
+    plt.plot(*vis['lower_ball'])
+    plt.plot(*vis['upper_ball'])
+    plt.plot(*vis['lever_arm'])
+    plt.axis('equal')
+    plt.show(block=False)
+    plt.pause(dt)
