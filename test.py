fe19be2e8b163d66398613ccae831d2615d62d54
ChristofDubs <christof.dubs@gmx.ch>
2018-05-03 12:04:22 +0200

    change test to check controller gains directly instead of comparing to original lqr controller

--- test.py
+++ test.py
@@ -2,12 +2,14 @@ import unittest
 
 import numpy as np
 
-import controller_2d as ctrl
+from controller_2d import Controller
 from definitions_2d import *
 
-lqr_controller = ctrl.LQRController()
-stacked_controller = ctrl.Controller()
+controller = Controller()
 delta = 1e-6
+
+K = np.array([0.447213596e+00, 1.03556079e+01, -4.73012271e+01,
+              3.683281576e+00, 6.05877477e-01, -3.53469304e+01])
 
 
 class TestController(unittest.TestCase):
@@ -16,23 +18,13 @@ class TestController(unittest.TestCase):
         for i in range(STATE_SIZE):
             x0 = np.zeros(STATE_SIZE)
             x0[i] = delta
-            u1 = lqr_controller.compute_ctrl_input(x0, 0)
-            u2 = stacked_controller.compute_ctrl_input(x0, 0)
-
-            k1 = u1 / delta
-            k2 = u2 / delta
-
-            self.assertAlmostEqual(k1, k2)
+            u = controller.compute_ctrl_input(x0, 0)
+            self.assertAlmostEqual(u / delta, -K[i])
 
         # input gain
         x0 = np.zeros(STATE_SIZE)
-        u1 = lqr_controller.compute_ctrl_input(x0, delta)
-        u2 = stacked_controller.compute_ctrl_input(x0, delta)
-
-        k1 = u1 / delta
-        k2 = u2 / delta
-
-        self.assertAlmostEqual(k1, k2)
+        u = controller.compute_ctrl_input(x0, delta)
+        self.assertAlmostEqual(u / delta, K[0])
 
 
 if __name__ == '__main__':
