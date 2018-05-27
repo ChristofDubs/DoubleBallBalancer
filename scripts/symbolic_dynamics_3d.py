b1cc2829e02b672110c2465bcd83dd3b13439e5d
ChristofDubs <christof.dubs@gmx.ch>
2018-05-27 17:11:34 +0200

    add lever arm to 3D model equation derivation script

--- scripts/symbolic_dynamics_3d.py
+++ scripts/symbolic_dynamics_3d.py
@@ -2,17 +2,19 @@
 
 Derivation of the rigid multi-body dynamics using the Projected Newton-Euler method.
 """
-from sympy import symbols, Matrix, sin, cos, simplify, solve, diff
+from sympy import symbols, Matrix, sin, cos, simplify, solve, diff, eye, diag, zeros
+from sympy.matrices.dense import rot_axis1, rot_axis2, rot_axis3
 
 # position
 x, y = symbols('x y')
 x_dot, y_dot = symbols('x_dot y_dot')
 
 # angles
-beta_x, beta_y, beta_z, psi_y, psi_z = symbols(
-    'beta_x, beta_y, beta_z psi_y psi_z')
+beta_x, beta_y, beta_z, phi_x, phi_y, phi_z, psi_y, psi_z = symbols(
+    'beta_x, beta_y, beta_z phi_x phi_y phi_z psi_y psi_z')
 
 # angular velocities
+phi_x_dot, phi_y_dot, phi_z_dot = symbols('phi_x_dot phi_y_dot phi_z_dot')
 psi_y_dot, psi_z_dot = symbols('psi_y_dot psi_z_dot')
 w_1x, w_1y, w_1z = symbols('w_1x w_1y w_1z')
 w_2x, w_2y, w_2z = symbols('w_2x w_2y w_2z')
@@ -21,18 +23,22 @@ omega_1 = Matrix([w_1x, w_1y, w_1z])
 omega_2 = Matrix([w_2x, w_2y, w_2z])
 
 # angular accelerations
-psi_y_ddot, psi_z_ddot, w_2_dot_x, w_2_dot_y, w_2_dot_z = symbols(
-    'psi_y_ddot psi_z_ddot w_2_dot_x w_2_dot_y w_2_dot_z')
+phi_x_ddot, phi_y_ddot, phi_z_ddot, psi_y_ddot, psi_z_ddot, w_2_dot_x, w_2_dot_y, w_2_dot_z = symbols(
+    'phi_x_ddot phi_y_ddot phi_z_ddot psi_y_ddot psi_z_ddot w_2_dot_x w_2_dot_y w_2_dot_z')
 
 # parameter
-m1, m2, r1, r2, theta1, theta2 = symbols('m1, m2, r1, r2, theta1, theta2')
+l, m1, m2, m3, r1, r2, theta1, theta2, theta3x, theta3y, theta3z = symbols(
+    'l m1 m2 m3 r1 r2 theta1 theta2 theta3x theta3y theta3z')
 
 # constants
 g = symbols('g')
 
+# inputs
+b_T = Matrix(symbols('Tx Ty Tz'))
+
 # parameter lists:
-m = [m1, m2]
-theta = [theta1, theta2]
+m = [m1, m2, m3]
+theta = [theta1 * eye(3), theta2 * eye(3), diag(theta3x, theta3y, theta3z)]
 
 # kinematic constraints: lower ball rolling on the ground
 r_OS1 = Matrix([x, y, r1])
@@ -69,41 +75,72 @@ omega_1[1] = simplify(sol[w_1y].subs(w_1z, omega_1[2]))
 
 sub_list = [(w_1x, omega_1[0]), (w_1y, omega_1[1]), (w_1z, omega_1[2])]
 
-# calculate Jacobians
+# eliminate omega_1
 v_OS1 = simplify(v_OS1.subs(sub_list))
 v_OS2 = simplify(v_OS2.subs(sub_list))
 
-v_i = [v_OS1, v_OS2]
-om_i = [omega_1, omega_2]
+# lever arm
+R_IB3 = rot_axis3(-phi_z) * rot_axis2(-phi_y) * rot_axis1(-phi_x)
+b_omega_3 = Matrix([phi_x_dot, 0, 0]) + rot_axis1(phi_x) * Matrix([0, phi_y_dot, 0]
+                                                                  ) + rot_axis1(phi_x) * rot_axis2(phi_y) * Matrix([0, 0, phi_z_dot])
+# print(b_omega_3.jacobian(Matrix([phi_x_dot, phi_y_dot, phi_z_dot])))
+# i_omega_3 = simplify(R_IB3 * b_omega_3)
+v_OS3 = v_OS2 + simplify(R_IB3 * b_omega_3.cross(Matrix([0, 0, -l])))
 
-omega_dot = Matrix([psi_y_ddot, psi_z_ddot, w_2_dot_x, w_2_dot_y, w_2_dot_z])
-omega = Matrix([psi_y_dot, psi_z_dot, w_2x, w_2y, w_2z])
-ang = Matrix([psi_y, psi_z, beta_x, beta_y, beta_z])
+
+# calculate Jacobians
+v_i = [v_OS1, v_OS2, v_OS3]
+om_i = [omega_1, omega_2, b_omega_3]
+
+omega_dot = Matrix([phi_x_ddot, phi_y_ddot, phi_z_ddot, psi_y_ddot,
+                    psi_z_ddot, w_2_dot_x, w_2_dot_y, w_2_dot_z])
+omega = Matrix([phi_x_dot, phi_y_dot, phi_z_dot, psi_y_dot, psi_z_dot, w_2x, w_2y, w_2z])
+ang = Matrix([phi_x, phi_y, phi_z, psi_y, psi_z, beta_x, beta_y, beta_z])
 
 J_i = [v.jacobian(omega) for v in v_i]
 JR_i = [om.jacobian(omega) for om in om_i]
 
 # Forces
 F_i = [Matrix([0, 0, -mi * g]) for mi in m]
-M_i = [Matrix([0, 0, 0]), Matrix([0, 0, 0])]
+M_i = [Matrix([0, 0, 0]), -R_IB3 * b_T, b_T]
 
 # Impulse
-p_i = [m[i] * v_i[i] for i in range(2)]
+p_i = [m[i] * v_i[i] for i in range(3)]
 p_dot_i = [p.jacobian(omega) * omega_dot + p.jacobian(ang) * omega for p in p_i]
 
 # Spin
-NS_i = [theta[i] * om_i[i] for i in range(2)]
-NS_dot_i = [NS.jacobian(omega) * omega_dot + NS.jacobian(ang) * omega for NS in NS_i]
+omega_diff_i = [
+    om_i[i].jacobian(omega) *
+    omega_dot +
+    om_i[i].jacobian(ang) *
+    omega for i in range(3)]
+NS_dot_i = [theta[i] * omega_diff_i[i] + om_i[i].cross(theta[i] * om_i[i]) for i in range(3)]
 
 # dynamics
-dyn = Matrix([0, 0, 0, 0, 0])
-for i in range(2):
+dyn = zeros(8, 1)
+for i in range(3):
     dyn += simplify(J_i[i].T * (p_dot_i[i] - F_i[i])) + simplify(JR_i[i].T * (NS_dot_i[i] - M_i[i]))
 
 A = simplify(dyn.jacobian(omega_dot))
 
-sub_list = [(x, 'self.p.' + x)
-            for x in ['g', 'l', 'm1', 'm2', 'm3', 'r1', 'r2', 'tau', 'theta1', 'theta2', 'theta3']]
+sub_list = [
+    (x,
+     'self.p.' +
+     x) for x in [
+        'g',
+        'l',
+        'm1',
+        'm2',
+        'm3',
+        'r1',
+        'r2',
+        'tau',
+        'theta1',
+        'theta2',
+        'theta3',
+        'theta3x',
+        'theta3y',
+        'theta3z']]
 for row in range(A.rows):
     for col in range(A.cols):
         if row > col and simplify(A[row, col] - A[col, row]) == 0:
