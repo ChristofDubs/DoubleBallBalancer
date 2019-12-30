eb167984f96b08623438fdde5c463f3eed71860f
ChristofDubs <christof.dubs@gmx.ch>
2019-12-30 08:06:05 +0100

    change to forward velocity controller, handle beta 2pi roll-over problem

--- model_3d/lqr_controller.py
+++ model_3d/lqr_controller.py
@@ -2,7 +2,6 @@
 """
 import numpy as np
 import context
-from pyrotation import Quaternion
 
 
 class LQRController(object):
@@ -10,22 +9,26 @@ class LQRController(object):
         self.K = np.array([[-26.67594832, 0., 4.36469406, 0., 4.36469403, 0., -25.83757989, 0., 3.52602561, 0., 0.28995756, 0.],
                            [0., -26.67594832, 0., 4.36469406, 0., 4.36469403, 0., -25.83757989, 0., 3.52602561, 0., 0.28995756]])
 
-    def compute_ctrl_input(self, state, beta_cmd):
+    def compute_ctrl_input(self, state, w2_y_cmd):
+
+        delta_phi = state.q3.get_roll_pitch_yaw()
+        beta_x = -state.phi_x + delta_phi[0]
+        beta_y = -state.phi_y + delta_phi[1]
+
+        w2 = state.omega_2
 
         # [psi_x, psi_y, beta_x, beta_y, phi_x, phi_y, psi_x_dot, psi_y_dot, w_2x, w_2y, phi_x_dot, phi_y_dot]
-        beta = Quaternion(state.q2).get_roll_pitch_yaw()
-        w2 = state.omega_2
         reduced_state = np.array([state.psi_x,
                                   state.psi_y,
-                                  beta[0],
-                                  beta[1],
+                                  beta_x,
+                                  beta_y,
                                   state.phi_x,
                                   state.phi_y,
                                   state.psi_x_dot,
                                   state.psi_y_dot,
                                   w2[0],
-                                  w2[1],
+                                  w2[1] - w2_y_cmd,
                                   state.phi_x_dot,
-                                  state.phi_y_dot])
+                                  state.phi_y_dot + w2_y_cmd])
 
         return -np.dot(self.K, reduced_state)
