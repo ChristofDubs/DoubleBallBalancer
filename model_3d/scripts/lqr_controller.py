0c61a7fd3337fbf58ea45317c973c3f4ba486714
ChristofDubs <christof.dubs@gmx.ch>
2020-02-22 11:29:01 +0100

    base lqr controller script on system matrices

--- model_3d/scripts/lqr_controller.py
+++ model_3d/scripts/lqr_controller.py
@@ -1,52 +1,22 @@
 """Script to compute LQR-controller gain for stabilizing the 3D Double Ball Balancer, based on numerical parameter values.
 """
 import numpy as np
-from scipy.linalg import solve_continuous_are
-from scipy.signal import place_poles
+from sympy import matrix2numpy
 import matplotlib.pyplot as plt
-
 import pickle
-
-from symbolic_dynamics_linearization import ang, omega, omega_dot, omega_cmd
 
 use_pole_placement = True
 
-dyn_lin = pickle.load(open("linear_dynamics.p", "rb"))
-
-# substitute parameters with numerical values
-dyn_lin = dyn_lin.subs("l", 1.0)
-dyn_lin = dyn_lin.subs("m1", 1.0)
-dyn_lin = dyn_lin.subs("m2", 1.0)
-dyn_lin = dyn_lin.subs("m3", 1.0)
-dyn_lin = dyn_lin.subs("r1", 3.0)
-dyn_lin = dyn_lin.subs("r2", 2.0)
-dyn_lin = dyn_lin.subs("theta1", 1.0)
-dyn_lin = dyn_lin.subs("theta2", 1.0)
-dyn_lin = dyn_lin.subs("theta3x", 1.0)
-dyn_lin = dyn_lin.subs("theta3y", 1.0)
-dyn_lin = dyn_lin.subs("theta3z", 1.0)
-dyn_lin = dyn_lin.subs("g", 9.81)
-dyn_lin = dyn_lin.subs("tau", 0.1)
-
-# extract matrices such that M*x_ddot + D*x_dot + K*x = Bd*u
-M = dyn_lin.jacobian(omega_dot)
-D = dyn_lin.jacobian(omega)
-K = dyn_lin.jacobian(ang)
-Bd = dyn_lin.jacobian(omega_cmd)
-
-# convert to first order x_ddot = A*x + B*u
-A = np.zeros([16, 16], dtype=float)
-
-A[:8, 8:] = np.eye(8)
-A[8:, :8] = np.array(-M.LUsolve(K))
-A[8:, 8:] = np.array(-M.LUsolve(D))
-
-B = np.zeros([16, 2], dtype=float)
-
-B[8:, :] = np.array(-M.LUsolve(Bd))
+[A, B] = pickle.load(open("system_matrices.p", "rb"))
+A = matrix2numpy(A, dtype=float)
+B = matrix2numpy(B, dtype=float)
 
 # eigenvalues
 print('open loop eigenvalues: \n{}'.format(np.linalg.eigvals(A)))
+
+# full state:
+# [alpha_z, psi_x, psi_y, beta_x, beta_y, beta_z, phi_x, phi_y]
+# [w_1z, psi_x_dot, psi_y_dot, w_2x, w_2y, w_2z, phi_x_dot, phi_y_dot]
 
 # remove uncontrollable states of the linear system:
 # angles: alpha_z, beta_z
@@ -61,6 +31,8 @@ A = np.delete(A, delete_mask, 1)
 B = np.delete(B, delete_mask, 0)
 
 if use_pole_placement:
+    from scipy.signal import place_poles
+
     # [beta, phi, psi, beta_dot, phi_dot, psi_dot]
     poles_2D = np.array([-2.07789478e+01 + 0.j, -1.23128756e-15 + 0.j, -1.48613821e+00 + 1.18812958j, -
                          1.48613821e+00 - 1.18812958j, -5.80068041e-01 + 0.27468173j, -5.80068041e-01 - 0.27468173j])
@@ -81,6 +53,8 @@ if use_pole_placement:
     K = place_poles(A, B, p, method='YT').gain_matrix
 
 else:
+    from scipy.linalg import solve_continuous_are
+
     # LQR controller:
     # https://en.wikipedia.org/wiki/Linear%E2%80%93quadratic_regulator
     R = np.eye(2, dtype=float) * 1
