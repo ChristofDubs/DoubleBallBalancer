a946d06d6b4d45b02e5d551721432d13cfdfa024
Christof Dubs <christof.dubs@gmx.ch>
2025-02-15 22:00:47 +0900

    rename gains to K

--- model_3d/scripts/controller_gain_fitting.py
+++ model_3d/scripts/controller_gain_fitting.py
@@ -25,15 +25,15 @@ for file in glob.glob('data/data*'):
 
     A = np.array([projectModelState(state)[0] for state in state_vec[:-1]])
 
-    # x = gains that reproduce the control signals us given the projected model state
-    x = np.linalg.solve(np.dot(A.T, A), np.dot(A.T, us))
+    # K = gains that reproduce the control signals u given the projected model state: u = K*x
+    K = np.linalg.solve(np.dot(A.T, A), np.dot(A.T, us))
 
     # plt.figure()
     # plt.plot(us, 'b*', label="desired u")
-    # plt.plot(np.dot(A, x), 'r-', label='linear fit')
+    # plt.plot(np.dot(A, K), 'r-', label='linear fit')
     # plt.legend()
     omega_2_y = projectModelState(state_vec[0])[1][3]
-    data.append([omega_2_y, x])
+    data.append([omega_2_y, K])
 
 us = [u for u, _ in data]
 # A = np.array([[1, u, u**2, u**3, u**4] for u in us])
@@ -42,9 +42,10 @@ A = np.array([[1, u**2, u**3, u**4, u**5, u**6] for u in us])
 # A = np.array([[1, u, u**2] for u in us])
 
 for i in range(len(data[0][1])):
-    b = np.array([x[i][0] for _, x in data])
+    b = np.array([K[i][0] for _, K in data])
     x = np.linalg.solve(np.dot(A.T, A), np.dot(A.T, b))
 
+    print('coefficients x that map f(u)*x = K_i')
     print(x)
 
     plt.figure()
